d6c79ad016bc2df0842a9baf3511f1afcdd1a8db
pppfkp <pppfkp@outlook.com>
2022-12-16 16:24:19 +0100

    Added DataAnnotations to the Movie model.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,13 +7,21 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
         [DataType(DataType.Date)]
         [DisplayName("Release Date")]
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; } = string.Empty;
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [Required]
+        [StringLength(5)]
         public string Rating { get; set; } = string.Empty;
     }
 }
